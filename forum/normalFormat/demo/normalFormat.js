8b47b891f48901fc60b592aa5bad6b5887e2a550
PatrickRyanMS <patricr@microsoft.com>
2020-01-22 15:31:09 -0800

    Adding babylon files

--- forum/normalFormat/demo/normalFormat.js
+++ forum/normalFormat/demo/normalFormat.js
@@ -6,49 +6,52 @@ var createScene = function () {
     var promises = [];
 
     // This creates and positions a free camera (non-mesh)
-    var camera = new BABYLON.ArcRotateCamera("ArcRotateCamera", BABYLON.Tools.ToRadians(-270), BABYLON.Tools.ToRadians(75), 110, new BABYLON.Vector3(0,1.5,0), scene);
+    var camera = new BABYLON.ArcRotateCamera("ArcRotateCamera", BABYLON.Tools.ToRadians(-270), BABYLON.Tools.ToRadians(90), 10, new BABYLON.Vector3(0,0,0), scene);
 
     // This attaches the camera to the canvas
     camera.attachControl(canvas, true);
     scene.clearColor = new BABYLON.Color3(0.1, 0.1, 0.1);
 
     // Create node materials
-    var swordNodeMat = new BABYLON.NodeMaterial("swordNodeMat", scene, { emitComments: false });
-    var gemNodeMat = new BABYLON.NodeMaterial("gemNodeMat", scene, { emitComments: false });
+    var directXMat = new BABYLON.NodeMaterial("directXMat", scene, { emitComments: false });
 
     // Lights
     var dirLight1 = new BABYLON.DirectionalLight("dirLight1", new BABYLON.Vector3(0, 0, 0), scene);
     dirLight1.direction = new BABYLON.Vector3(0.5, -0.75, -0.4);
-    dirLight1.intensity = 0.95;
-    dirLight1.diffuse = new BABYLON.Color3(0.8431372549019608, 0.8431372549019608, 0.8431372549019608);
-    dirLight1.specular = new BABYLON.Color3(0.7568627450980392, 0.7568627450980392, 0.7568627450980392);
+    dirLight1.intensity = 1.0;
+
+    var planeMesh = BABYLON.MeshBuilder.CreatePlane("planeMesh", {width: 5, height: 5}, scene);
+    planeMesh.position = new BABYLON.Vector3(0, 6, 0);
+    planeMesh.rotation.y = Math.PI;
+    var planeMat = new BABYLON.StandardMaterial("planeMat", scene);
+    planeMat = new BABYLON.Texture("https://raw.githubusercontent.com/PatrickRyanMS/BabylonJStextures/master/forum/normalFormat/directX_normal.png")
+    planeMesh.material = planeMat;
 
     // Load assets 
-    // promises.push(BABYLON.SceneLoader.AppendAsync("https://raw.githubusercontent.com/PatrickRyanMS/BabylonJStextures/master/Demos/treeScene/treeScene.glb"));
-    promises.push(BABYLON.SceneLoader.AppendAsync("assets/sword_noMat.glb"));
-    promises.push(gemNodeMat.loadAsync("assets/gemNodeShader.json"));
-    promises.push(swordNodeMat.loadAsync("assets/swordNodeShader.json"));
+    // promises.push(BABYLON.SceneLoader.AppendAsync(""));
+    promises.push(BABYLON.SceneLoader.AppendAsync("https://raw.githubusercontent.com/PatrickRyanMS/BabylonJStextures/master/forum/normalFormat/normalFormatTest.glb"));
+    promises.push(BABYLON.SceneLoader.AppendAsync("https://raw.githubusercontent.com/PatrickRyanMS/BabylonJStextures/master/forum/normalFormat/normalFormatTest.babylon"));
+    promises.push(directXMat.loadAsync("https://raw.githubusercontent.com/PatrickRyanMS/BabylonJStextures/master/forum/normalFormat/directXnodeMat.json"));
 
     // Callback when assets are loaded
     Promise.all(promises).then(function() {
 
         // Meshes
-        swordMesh = scene.getMeshByName("sword_low");
-        gemMesh = scene.getMeshByName("handleGem_low");
-        
-        //Build and assign node materials
-        swordNodeMat.build(true);
-        swordMesh.material = swordNodeMat;
+        directXMesh = scene.getMeshByName("directX");
 
-        gemNodeMat.build(true);
-        gemMesh.material = gemNodeMat;
+        //Build and assign node materials
+        directXMat.build(true);
+        directXMesh.material = directXMat;
+
+        // display loading screen while loading assets
+        engine.displayLoadingUI();  
+        scene.executeWhenReady(function() {
+            engine.hideLoadingUI();
+        });
     });
 
     scene.debugLayer.show();
 
-    // lock camera beta angle
-    scene.registerBeforeRender(function () {
-    });
-
     return scene;
 };
+
